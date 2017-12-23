00bd64734673fb7ace0feb8b9e1d182491af1fdb
sunxy <736870598@qq.com>
2017-12-24 00:14:01 +0800

    jpeg库增加了86的支持，下哦i个了预览图片是可能出现的不过，增加了时间日期选择Dialog功能。

---
+++ PhotoCore/src/main/java/com/sunxiaoyu/photocore/core/photoview/ViewPagerFix.java
@@ -0,0 +1,41 @@
+package com.sunxiaoyu.photocore.core.photoview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by SunXiaoyu on 2017/12/16/016.
+ */
+
+public class ViewPagerFix extends ViewPager {
+
+    public ViewPagerFix(Context context) {
+        super(context);
+    }
+
+    public ViewPagerFix(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
